a8d894db191957a303a8baac3393df1498170869
kevyn <kevyn.buchet@rosemood.fr>
2022-04-14 14:55:49 +0200

    backup

--- app/src/main/java/companion/battery/ady/MainViewModel.kt
+++ app/src/main/java/companion/battery/ady/MainViewModel.kt
@@ -5,18 +5,16 @@ import android.app.Application
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
-import android.widget.Toast
 import androidx.annotation.RequiresPermission
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
 //region Properties
 
-    private val bluetoothManager: BluetoothManager = application.applicationContext.getSystemService(BluetoothManager::class.java)
-    val bluetoothAdapter: BluetoothAdapter = bluetoothManager.adapter
+    private val bluetoothManager: BluetoothManager? = ContextCompat.getSystemService(BatteryCompanionApp.context, BluetoothManager::class.java)
+    private val bluetoothAdapter: BluetoothAdapter? = bluetoothManager?.adapter
 
 //endregion
 
@@ -31,8 +29,8 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
     fun getBluetoothDevices() {
 
-        val bluetoothManager: BluetoothManager? = ContextCompat.getSystemService(BatteryCompanionApp.context, BluetoothManager::class.java)
-        val bluetoothAdapter: BluetoothAdapter = bluetoothManager?.adapter ?: return
+        if (bluetoothAdapter == null)
+            return
 
         devices.clear()
         devices.addAll(bluetoothAdapter.bondedDevices)
