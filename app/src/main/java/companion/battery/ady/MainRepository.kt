44a39e390ec6d6b2da9a49c6fa727a6d3b52cb17
kevyn <kevyn.buchet@rosemood.fr>
2022-04-15 16:16:59 +0200

    backup

--- app/src/main/java/companion/battery/ady/MainRepository.kt
+++ app/src/main/java/companion/battery/ady/MainRepository.kt
@@ -3,23 +3,10 @@ package companion.battery.ady
 import android.Manifest
 import android.bluetooth.BluetoothManager
 import androidx.annotation.RequiresPermission
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import companion.battery.ady.extensions.isConnected
 import javax.inject.Inject
 
 class MainRepository @Inject constructor() {
-
-    private val mDevice: MediatorLiveData<Device> = MediatorLiveData<Device>()
-    val data: LiveData<Device> = mDevice
-
-    fun addDataSource(data: LiveData<Device>) {
-        mDevice.addSource(data) { mDevice.value = it }
-    }
-
-    fun removeDataSource(data: LiveData<Device>) {
-        mDevice.removeSource(data)
-    }
 
     @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
     fun getBluetoothDevices(manager: BluetoothManager?) : List<Device> {
