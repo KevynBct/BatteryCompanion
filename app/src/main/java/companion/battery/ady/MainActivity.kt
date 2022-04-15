73398ed100d3ecad116bdc3dd50bd01c4d489edb
kevyn <kevyn.buchet@rosemood.fr>
2022-04-15 16:13:56 +0200

    move files

--- app/src/main/java/companion/battery/ady/MainActivity.kt
+++ app/src/main/java/companion/battery/ady/MainActivity.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
+import companion.battery.ady.ui.composables.MainContent
 import companion.battery.ady.ui.theme.BatteryCompanionTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -151,98 +152,3 @@ class MainActivity : ComponentActivity() {
 //endregion
 
 }
-
-//region Composable
-
-@Composable
-fun MainContent(onRetryButtonTap: () -> Unit) {
-
-    BatteryCompanionTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-
-            Content(onRetryButtonTap = onRetryButtonTap)
-
-        }
-    }
-
-}
-
-@SuppressLint("MissingPermission")
-@Composable
-fun Content(
-    onRetryButtonTap: () -> Unit,
-    viewModel: MainViewModel = viewModel()
-) {
-
-    val devices by viewModel.devices.observeAsState(initial = emptyList())
-
-    Column(
-        modifier = Modifier
-            .statusBarsPadding()
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-    ) {
-
-        if (devices.isEmpty()) {
-
-            Button(
-                onClick = { onRetryButtonTap() },
-                content = {
-                    Text(
-                        text = "Actualiser",
-                        color = MaterialTheme.colorScheme.primary
-                    )
-                }
-            )
-
-        } else {
-
-            devices.forEach { BluetoothDeviceItem(device = it) }
-
-        }
-
-        Spacer(modifier = Modifier.navigationBarsPadding())
-
-    }
-
-
-}
-
-@SuppressLint("MissingPermission")
-@Composable
-fun BluetoothDeviceItem(device: Device) {
-
-    Column(
-        modifier = Modifier
-            .alpha(if (device.isConnected) 1f else .5f)
-            .fillMaxWidth()
-            .padding(all = 8.dp)
-            .clip(RoundedCornerShape(8.dp))
-            .border(
-                width = 1.dp,
-                color = MaterialTheme.colorScheme.tertiary,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .padding(horizontal = 24.dp, vertical = 16.dp)
-    ) {
-
-        Text(
-            text = device.name,
-            color = MaterialTheme.colorScheme.primary,
-            fontSize = 20.sp
-        )
-
-        Text(
-            text = device.name,
-            color = MaterialTheme.colorScheme.secondary
-        )
-
-    }
-
-}
-
-//endregion
