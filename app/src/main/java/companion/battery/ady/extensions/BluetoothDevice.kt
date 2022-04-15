272a8cd7786c120edd271e88815b835a2098076e
kevyn <kevyn.buchet@rosemood.fr>
2022-04-15 15:59:09 +0200

    shorter method

--- app/src/main/java/companion/battery/ady/extensions/BluetoothDevice.kt
+++ app/src/main/java/companion/battery/ady/extensions/BluetoothDevice.kt
@@ -1,13 +1,6 @@
 package companion.battery.ady.extensions
 
 import android.bluetooth.BluetoothDevice
-import android.util.Log
 
 val BluetoothDevice.isConnected: Boolean
-    get() = try {
-        val method = this.javaClass.getMethod("isConnected")
-        method.invoke(this) as Boolean
-    } catch (e: Exception) {
-        Log.i("TEST_", "error : $e")
-        false
-    }
+    get() = try { this.javaClass.getMethod("isConnected").invoke(this) as Boolean } catch (e: Exception) { false }
