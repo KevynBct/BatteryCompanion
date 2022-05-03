b9283bf714cdb6a187099f3e8086a940f0ef88ef
kevyn <kevyn.buchet@rosemood.fr>
2022-05-03 08:57:05 +0200

    move items

---
+++ app/src/main/java/companion/battery/ady/ui/composables/Items.kt
@@ -0,0 +1,132 @@
+package companion.battery.ady.ui.composables
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothClass
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import companion.battery.ady.model.Device
+
+@ExperimentalMaterial3Api
+@SuppressLint("MissingPermission")
+@Composable
+fun DeviceWithBatteryItem(modifier: Modifier, device: Device) {
+
+    Card(
+        modifier = Modifier
+            .then(modifier)
+            .padding(vertical = 8.dp)
+            .background(
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(horizontal = 24.dp, vertical = 16.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+
+        Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+
+            SurfaceText(
+                text = device.name,
+                fontSize = 17.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                contentAlignment = Alignment.Center
+            ) {
+
+                val color = if (device.battery <= 20) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.primary
+
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f),
+                    progress = device.battery / 100f,
+                    color = color,
+                    strokeWidth = 4.dp
+                )
+
+                SurfaceText(
+                    text = "${device.battery} %"
+                )
+
+
+            }
+
+        }
+
+    }
+
+}
+
+@ExperimentalMaterial3Api
+@SuppressLint("MissingPermission")
+@Composable
+fun DeviceWithoutBatteryItem(device: Device) {
+
+    Card(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .background(
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(horizontal = 24.dp, vertical = 16.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            SurfaceText(
+                text = device.name,
+                fontSize = 17.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Column {
+
+                val icon = when (device.majorDeviceClass) {
+                    BluetoothClass.Device.Major.AUDIO_VIDEO -> Icons.Outlined.Headphones
+                    BluetoothClass.Device.Major.PHONE -> Icons.Outlined.Phone
+                    BluetoothClass.Device.Major.WEARABLE -> Icons.Outlined.Watch
+                    BluetoothClass.Device.Major.HEALTH -> Icons.Outlined.HealthAndSafety
+                    BluetoothClass.Device.Major.COMPUTER -> Icons.Outlined.Computer
+                    else -> Icons.Outlined.Bluetooth
+                }
+
+                Icon(
+                    modifier = Modifier.size(15.dp),
+                    imageVector = icon,
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    contentDescription = null
+                )
+
+            }
+
+        }
+
+    }
+
+}
--- app/src/main/java/companion/battery/ady/ui/composables/MainComposables.kt
+++ app/src/main/java/companion/battery/ady/ui/composables/MainComposables.kt
@@ -1,19 +1,11 @@
 package companion.battery.ady.ui.composables
 
-import android.annotation.SuppressLint
-import android.bluetooth.BluetoothClass
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.*
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -21,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,7 +21,6 @@ import com.google.accompanist.flowlayout.FlowMainAxisAlignment
 import com.google.accompanist.flowlayout.FlowRow
 import com.google.accompanist.flowlayout.SizeMode
 import companion.battery.ady.MainViewModel
-import companion.battery.ady.model.Device
 import companion.battery.ady.ui.theme.BatteryCompanionTheme
 
 @Composable
@@ -141,117 +131,6 @@ fun DevicesList(viewModel: MainViewModel = viewModel()) {
             ColorItemList()
 
             Spacer(modifier = Modifier.size(40.dp))
-
-        }
-
-    }
-
-}
-
-@ExperimentalMaterial3Api
-@SuppressLint("MissingPermission")
-@Composable
-fun DeviceWithBatteryItem(modifier: Modifier, device: Device) {
-
-    Card(
-        modifier = Modifier
-            .then(modifier)
-            .padding(vertical = 8.dp)
-            .background(
-                color = MaterialTheme.colorScheme.surfaceVariant,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .padding(horizontal = 24.dp, vertical = 16.dp),
-        shape = RoundedCornerShape(8.dp)
-    ) {
-
-        Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-            
-            SurfaceText(
-                text = device.name,
-                fontSize = 17.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1f),
-                contentAlignment = Alignment.Center
-            ) {
-
-                val color = if (device.battery <= 20) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.primary
-
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(1f),
-                    progress = device.battery / 100f,
-                    color = color,
-                    strokeWidth = 4.dp
-                )
-
-                SurfaceText(
-                    text = "${device.battery} %"
-                )
-
-
-            }
-
-        }
-
-    }
-
-}
-
-@ExperimentalMaterial3Api
-@SuppressLint("MissingPermission")
-@Composable
-fun DeviceWithoutBatteryItem(device: Device) {
-
-    Card(
-        modifier = Modifier
-            .padding(vertical = 8.dp)
-            .background(
-                color = MaterialTheme.colorScheme.surfaceVariant,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .padding(horizontal = 24.dp, vertical = 16.dp),
-        shape = RoundedCornerShape(8.dp)
-    ) {
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-
-            SurfaceText(
-                text = device.name,
-                fontSize = 17.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-
-            Column {
-
-                val icon = when (device.majorDeviceClass) {
-                    BluetoothClass.Device.Major.AUDIO_VIDEO -> Icons.Outlined.Headphones
-                    BluetoothClass.Device.Major.PHONE -> Icons.Outlined.Phone
-                    BluetoothClass.Device.Major.WEARABLE -> Icons.Outlined.Watch
-                    BluetoothClass.Device.Major.HEALTH -> Icons.Outlined.HealthAndSafety
-                    BluetoothClass.Device.Major.COMPUTER -> Icons.Outlined.Computer
-                    else -> Icons.Outlined.Bluetooth
-                }
-
-                Icon(
-                    modifier = Modifier.size(15.dp),
-                    imageVector = icon,
-                    tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                    contentDescription = null
-                )
-
-            }
 
         }
 
