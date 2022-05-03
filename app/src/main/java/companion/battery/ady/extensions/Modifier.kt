9e2022064298fe80a45cc2b948b3ab3132e5155f
kevyn <kevyn.buchet@rosemood.fr>
2022-05-03 16:44:34 +0200

    Modifier.mirror

--- app/src/main/java/companion/battery/ady/extensions/Modifier.kt
+++ app/src/main/java/companion/battery/ady/extensions/Modifier.kt
@@ -1,16 +1,10 @@
 package companion.battery.ady.extensions
 
-import android.util.LayoutDirection
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.core.text.layoutDirection
-import java.util.*
 
 @Stable
 fun Modifier.mirror(): Modifier {
-    return if (Locale.getDefault().layoutDirection == LayoutDirection.RTL)
-        this.scale(scaleX = -1f, scaleY = 1f)
-    else
-        this
+    return this.scale(scaleX = -1f, scaleY = 1f)
 }
